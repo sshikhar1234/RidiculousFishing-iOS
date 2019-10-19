de8c50fee8256161d11621b89d3f81010ba76b64
sshikhar1234 <sshikharshah@gmail.com>
2019-10-19 01:44:22 -0400

    Setup the game background and game start condition

--- RidiculousFishing/GameScene.swift
+++ RidiculousFishing/GameScene.swift
@@ -14,27 +14,26 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    private let SPEED:CGFloat = 250
+    
+    private var ship : SKSpriteNode!
+    private var hook : SKSpriteNode!
+    private var bg_occean : SKSpriteNode!
+    private var sky : SKSpriteNode!
+        
+        
+    
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //1. Initialize the sprites
+        self.ship = self.childNode(withName: "ship") as! SKSpriteNode
+        self.hook = self.childNode(withName: "hook") as! SKSpriteNode
+        self.bg_occean = self.childNode(withName: "bg_occean") as! SKSpriteNode
+        self.sky = self.childNode(withName: "sky") as! SKSpriteNode
+                                     
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
     }
     
     
@@ -53,7 +52,7 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
+
     func touchUp(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -61,10 +60,42 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let mouseTouch = touches.first
+                if( mouseTouch == nil){
+                    return
+                }
+        let location = mouseTouch!.location(in: self)
+        let nodeTouched = atPoint(location).name
+        if(nodeTouched == "ship"){
+            
+            //TODO: Animate the move effect when the player taps on Ship(GAME START)
+            
+            let moveUpAnimation: SKAction
+            let moveDownAnimation: SKAction
+            moveUpAnimation = SKAction.moveBy(x: 0, y: self.SPEED, duration: 1)
+            
+            //1. Move the sky up
+            self.sky.run(moveUpAnimation)
+            
+            //2. Move the occean up
+            self.bg_occean.run(moveUpAnimation)
+
+            //3. Move the ship up
+            self.ship.run(moveUpAnimation)
+
+            //4. Move the hook down
+            moveDownAnimation = SKAction.moveBy(x: 0, y: -10, duration: 1.5)
+            self.hook.run(moveDownAnimation)
+
+        }
+        
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            
+
         }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
@@ -85,5 +116,7 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        
     }
 }
